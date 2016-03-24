128b30d6884ecb3906f7f6e1245ab76348d86620
Amber <Amber.Dang@morningstar.com>
2016-03-24 10:51:28 +0800

    add client side test code

--- NodejsSrv.js
+++ NodejsSrv.js
@@ -4,65 +4,35 @@ var url = require("url");
 var lisFn = function(req, rsp){
 	var actType = "";
 	var actTarUrl = "";
-	var isInputData = false;
-	var bufRef;
-	/*req.pipe(rsp);*/
+	var bufArr=[];//buffer object container
 	
-	console.log("request's method: " + req.method);
-	console.log("request's URL: " + req.url);
-	/*console.log("request's socket: " + req.socket);
-	console.log("request's header object: ");
-	for(var p in req.headers)
-		console.log("\t" + p + ": " + req.headers[p]);*/
-	/*console.log("Request Raw Header: ");
-	for(var i = 0, len = req.rawHeaders.length; i < len; i+=2)
-		console.log(req.rawHeaders[i] + ": " + req.rawHeaders[i+1]);*/
 	var qObj = url.parse(req.url, true).query;
 	actType = qObj.action || "get";
 	actTarUrl = qObj.dsturl || "http://www.baidu.com/";
-	isInputData = (qObj.bdata === "1") ? true : false;
+	console.log("request's URL: " + req.url);
+	console.log("parameter [action]: " + actType);
+	console.log("parameter [dsturl]: " + actTarUrl);
 	console.log("\n");
 	
+	req.on('data', function(chunk){bufArr.push(chunk);//push an Buffer object ot Array.});
+	req.on('end', function(){
+		var wholeBuf;
+		var reqStr = "";
+		var reqJsonObj = "root:{}";
+		if(bufArr.length > 0)
+		{
+			wholeBuf = Buffer.concat(bufArr);//Buffer.concat() function concat serveral buffer object to a new buffer object.
+			bufArr = [];//release the tmporay array.
+			reqStr = wholeBuf.toString('utf8');
+			reqJsonObj = JSON.parse(reqStr);
+		}
+		console.log(wholeBuf);
+		console.log(reqStr);
+		console.log(reqJsonObj);
+		rsp.write('<p>Hello, Your <span style="font-weight:bold;">[' + actType + ']</span> request for <span style="font-weight:bold;">[' + actTarUrl + ']</span> with data <span style="font-weight:bold;">[' + reqStr + '] start on proxy server now.</p>');
+		
+	});
 	
-	
-	if(isInputData)
-	{
-		req.on('data', function(chunk){
-			bufRef = Buffer.concat([bufRef,chunk]);
-		});
-		req.on('end', function(){
-			var reqStr = "";
-			var reqJsonObj = "root:{}";
-			if(bufRef)
-			{
-				reqStr = bufRef.toString('utf8');
-				reqJsonObj = JSON.parse(reqStr);
-			}
-			
-			console.log(bufRef);
-			console.log(reqStr);
-			console.log(reqJsonObj);
-			
-			//send request to baidu.com
-			//get response to response
-			rsp.write('<p>Hello, Your <span style="font-weight:bold;">[' + actType + ']</span> request for <span style="font-weight:bold;">[' + actTarUrl + ']</span> with data <span style="font-weight:bold;">[' + reqStr + '] will send back to replace this</p>');
-		});
-	}
-	else
-	{
-		//send request to baidu.com
-		//get response to response
-		rsp.write('<p>Hello, Your <span style="font-weight:bold;">[' + actType + ']</span> request for <span style="font-weight:bold;">[' + actTarUrl + ']</span> will send back to replace this</p>');
-		var dstObj = url.parse(actTarUrl);
-		var proReq = http.request({
-			host: dstObj.host,
-			method: actType.toUpperCase(),
-			path: dstObj.path
-		}, function(dstRsp){
-			dstRsp.pipe(rsp);
-		});
-		proReq.end();
-	}
 	req.on('close', function(){
 		delete bufRef;
 	});
---
+++ proxyAjax.js
@@ -0,0 +1,12 @@
+function nodeAjax(action, url, senddata, parseFn)
+{
+    var a = new XMLHttpRequest();
+    var proxyServer = "http://localhost:8080/"; //my local nodejs server.
+    a.open("POST", proxyServer+"?action=" + action + "&dsturl="+url, true); //open(method, url, async)
+    a.onreadystatechange = parseFn;
+    a.send(senddata || null);
+}
+
+nodeAjax("get", "http://www.baidu.com/", null, function(){
+	console.log(this);
+})
